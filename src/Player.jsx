00c04eca37b6345cf0fb7ca8268c95862b80601f
Brendan West <brendantwest@gmail.com>
2024-04-22 08:17:48 -0400

    Added dynamic tally logic

--- src/Player.jsx
+++ src/Player.jsx
@@ -1,17 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPalette } from "@fortawesome/free-solid-svg-icons";
 
 function Player({ initialBgColor, flipped }) {
   const [life, setLife] = useState(40);
   const [bgColor, setBgColor] = useState(initialBgColor);
+  const [changeTally, setChangeTally] = useState(0);
+  const [showTally, setShowTally] = useState(false);
+  const [showTallyClass, setShowTallyClass] = useState("");
+
+  let tallyTimeout = useRef(null);
+
+  const manageTallyDisplay = (change) => {
+    clearTimeout(tallyTimeout.current);
+    setChangeTally((prev) => prev + change);
+    setShowTally(true);
+    setShowTallyClass(""); // Ensure class does not include 'hidden' on initial show
+
+    tallyTimeout.current = setTimeout(() => {
+      setShowTallyClass("hidden"); // First, trigger the fade-out effect
+      setTimeout(() => {
+        setShowTally(false);
+        setChangeTally(0); // Reset the tally after it's no longer visible
+      }, 500); // This timeout matches the duration of the opacity transition
+    }, 1000); // Time visible before beginning to fade out
+    console.log();
+  };
 
   const decreaseLife = () => {
     setLife((prevLife) => prevLife - 1);
+    manageTallyDisplay(-1);
   };
 
   const increaseLife = () => {
     setLife((prevLife) => prevLife + 1);
+    manageTallyDisplay(+1);
   };
 
   const backgroundColors = [
@@ -40,13 +63,25 @@ function Player({ initialBgColor, flipped }) {
     transform: flipped ? "scaleX(-1)" : "none,",
   };
 
+  useEffect(() => {
+    return () => {
+      clearTimeout(tallyTimeout.current); // Clear the timeout if component unmounts
+    };
+  }, []);
+
   return (
     <div className={`Player ${flipped ? "flipped" : ""}`} style={playerStyle}>
+      {showTally && (
+        <div className={`ChangeTally ${showTallyClass}`}>
+          {changeTally > 0 ? `+${changeTally}` : changeTally}
+        </div>
+      )}
       <div className="Health">
         <div
           className={flipped ? "Increase" : "Decrease"}
           onClick={flipped ? increaseLife : decreaseLife}
         ></div>
+
         <div className="Life" style={lifeStyle}>
           {life}
         </div>
